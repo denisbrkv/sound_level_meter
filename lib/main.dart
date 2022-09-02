59d1c50315cb76adc2ed1fb77f7761f96c0f9750
denisbrkv <buryakovdv@gmail.com>
2022-09-02 15:33:29 +0530

    add SoundLevelView

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,53 @@ class SoundLevelMeter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Ghbdtn'));
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Placeholder(
+            color: Colors.grey,
+            fallbackHeight: MediaQuery.of(context).size.height * 0.3,
+          ),
+        ),
+        SizedBox(height: 10),
+        IntrinsicHeight(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '43.1',
+                style: TextStyle(
+                    fontSize: 56,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500),
+              ),
+              const VerticalDivider(
+                thickness: 1,
+                color: Colors.grey,
+                indent: 5,
+                endIndent: 5,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Min: 35.1',
+                    style: TextStyle(fontSize: 18, color: Colors.green),
+                  ),
+                  Text(
+                    'Max: 84.2',
+                    style: TextStyle(fontSize: 18, color: Colors.red),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
